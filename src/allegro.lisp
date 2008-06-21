4534ae9440d3efd1cf502d4767de7fe22defde16
Greg Pfeil <greg@technomadic.org>
2008-06-21 14:46:18 -0400

    Minor simplification of TYPECASE forms.
    darcs-hash:20080621184618-f3158-680de8011351be8b7b778dc37da877d3f196fab2.gz

--- src/allegro.lisp
+++ src/allegro.lisp
@@ -11,10 +11,7 @@
 (defmethod run (program args &key input if-input-does-not-exist output if-output-exists error if-error-exists)
   (let ((input-stream (etypecase input
                         (stream input)
-                        (pathname
-                         (open input
-                               :if-does-not-exist if-input-does-not-exist))
-                        (string
+                        ((or pathname string)
                          (open input
                                :if-does-not-exist if-input-does-not-exist))
                         (null nil)
@@ -25,12 +22,7 @@
                                  :input input-stream)
       (typecase output
         (stream (write-sequence output-string output))
-        (pathname
-         (with-open-file (out output
-                              :direction :output
-                              :if-exists if-output-exists)
-           (write-sequence output-string out)))
-        (string
+        ((or pathname string)
          (with-open-file (out output
                               :direction :output
                               :if-exists if-output-exists)
@@ -38,12 +30,7 @@
         (boolean (and output (write-sequence output-string *standard-output*))))
       (typecase error
         (stream (write-sequence error-string error))
-        (pathname
-         (with-open-file (err error
-                              :direction :output
-                              :if-exists if-error-exists)
-           (write-sequence error-string err)))
-        (string
+        ((or pathname string)
          (with-open-file (err error
                               :direction :output
                               :if-exists if-error-exists)
