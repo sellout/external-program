18397506915e0ce702b92f1aa7398f5c591facc7
Greg Pfeil <greg@technomadic.org>
2011-01-08 23:29:19 -0500

    Tests are now integrated with ASDF and use FIVEAM.

--- external-program.asd
+++ external-program.asd
@@ -10,23 +10,34 @@
   :author "Greg Pfeil <greg@technomadic.org>"
   :licence "LLGPL"
   :version "0.0.6"
-  :components
-  ((:module "src"
-            :components
-            ((:file "external-program")
-             (:file #+allegro "allegro"
-                    #+armedbear "armedbear"
-                    #+clisp "clisp"
-                    #+cmu "cmucl"
-                    #+ecl "ecl"
-                    ;; #+gcl "gcl"
-                    ;; #+liquid "liquid"
-                    #+lispworks "lispworks"
-                    ;; #+lucid "lucid"
-                    #+openmcl "openmcl"
-                    #+sbcl "sbcl"
-                    ;; #+scl "scieneer"
-                    #-(or allegro armedbear clisp cmu ecl lispworks openmcl
-                          sbcl)
-                    "unsupported"
-                    :depends-on ("external-program"))))))
+  :pathname "src/"
+  :components ((:file "external-program")
+               (:file #+allegro "allegro"
+                      #+armedbear "armedbear"
+                      #+clisp "clisp"
+                      #+cmu "cmucl"
+                      #+ecl "ecl"
+                      ;; #+gcl "gcl"
+                      ;; #+liquid "liquid"
+                      #+lispworks "lispworks"
+                      ;; #+lucid "lucid"
+                      #+openmcl "openmcl"
+                      #+sbcl "sbcl"
+                      ;; #+scl "scieneer"
+                      #-(or allegro armedbear clisp cmu ecl lispworks openmcl
+                            sbcl)
+                      "unsupported"
+                      :depends-on ("external-program")))
+  :in-order-to ((test-op (load-op external-program-test)))
+  :perform (test-op :after (op c)
+                    (funcall (intern "RUN!" :external-program-tests)
+                             (intern "TESTS" :external-program-tests))))
+
+(defmethod operation-done-p
+    ((op test-op) (c (eql (find-system :external-program))))
+  (values nil))
+
+(defsystem external-program-test
+  :depends-on (external-program fiveam)
+  :pathname "tests/"
+  :components ((:file "tests")))
--- tests/tests.lisp
+++ tests/tests.lisp
@@ -1,43 +1,54 @@
 (defpackage external-program-tests
-  (:use #:cl #:external-program))
-;;; FIXME: need to add a testing framework
+  (:use #:cl #:external-program #:fiveam)
+  (:shadow #:run))
 
 (in-package #:external-program-tests)
+
+(def-suite tests)
+
+(in-suite tests)
 
 ;;; FIXME: should probably signal a condition if program isn't found
 ;;; ... but can't guarantee that 71 isn't returned by the program
 ;;; itself ...
-(defun should-not-have-access-to-shell-builtins ()
+(test should-not-have-access-to-shell-builtins
   (multiple-value-bind (status code)
       (external-program:run "source" '("foo"))
-    (and (eq status :exited) (= code 71))))
+    (is (eq status :exited))
+    (is (= code 71))))
 
-(defun should-discover-programs-in-path ()
+(test should-discover-programs-in-path
   (multiple-value-bind (status code)
       (external-program:run "which" '("which"))
-    (and (eq status :exited) (= code 0))))
+    (is (eq status :exited))
+    (is (= code 0))))
 
-(defun should-be-able-to-use-pathname-as-program ()
+(test should-be-able-to-use-pathname-as-program
   (multiple-value-bind (status code)
       (external-program:run (make-pathname :name "which") '("which"))
-    (and (eq status :exited) (= code 0))))
+    (is (eq status :exited))
+    (is (= code 0))))
 
-(defun should-be-able-to-use-pathnames-as-args ()
+(test should-be-able-to-use-pathnames-as-args
   (multiple-value-bind (status code)
       (external-program:run "which" (list (make-pathname :name "which")))
-    (and (eq status :exited) (= code 0))))
+    (is (eq status :exited))
+    (is (= code 0))))
 
-(defun should-be-able-to-use-numbers-as-args ()
+(test should-be-able-to-use-numbers-as-args
   (multiple-value-bind (status code)
       (external-program:run "grep" (list "-C" 3 "should" (path-to-file)))
-    (and (eq status :exited) (= code 0))))
+    (is (eq status :exited))
+    (is (= code 0))))
 
-(defun environment-vars-should-override-existing ()
+(test environment-vars-should-override-existing
   (multiple-value-bind (status code)
       (external-program:run "which" '("which") :environment '(("PATH" . "")))
-    (and (eq status :exited) (/= code 0))))
+    (is (eq status :exited))
+    (is (/= code 0))))
 
-(defun empty-env-should-erase-all ()
+(test empty-env-should-erase-all
   (multiple-value-bind (status code)
       (external-program:run "which" '("which") :replace-environment-p t)
-    (and (eq status :exited) (= code 71))))
+    (is (eq status :exited))
+    (is (= code 71))))
