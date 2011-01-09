a6d54b825e7bd533436b24217cbd4a41c64616f8
Greg Pfeil <greg@technomadic.org>
2011-01-09 09:45:11 -0500

    Reversed the args for all checks, so that the expected value comes first, giving better failure reporting.

--- tests/tests.lisp
+++ tests/tests.lisp
@@ -14,42 +14,42 @@
 (test should-not-have-access-to-shell-builtins
   (multiple-value-bind (status code)
       (external-program:run "source" '("foo"))
-    (is (eq status :exited))
-    (is (= code 71))))
+    (is (eq :exited status))
+    (is (= 71 code))))
 
 (test should-discover-programs-in-path
   (multiple-value-bind (status code)
       (external-program:run "which" '("which"))
-    (is (eq status :exited))
-    (is (= code 0))))
+    (is (eq :exited status))
+    (is (= 0 code))))
 
 (test should-be-able-to-use-pathname-as-program
   (multiple-value-bind (status code)
       (external-program:run (make-pathname :name "which") '("which"))
-    (is (eq status :exited))
-    (is (= code 0))))
+    (is (eq :exited status))
+    (is (= 0 code))))
 
 (test should-be-able-to-use-pathnames-as-args
   (multiple-value-bind (status code)
       (external-program:run "which" (list (make-pathname :name "which")))
-    (is (eq status :exited))
-    (is (= code 0))))
+    (is (eq :exited status))
+    (is (= 0 code))))
 
 (test should-be-able-to-use-numbers-as-args
   (multiple-value-bind (status code)
       (external-program:run "grep"
                             (list "-C" 3 "should" #.*compile-file-truename*))
-    (is (eq status :exited))
-    (is (= code 0))))
+    (is (eq :exited status))
+    (is (= 0 code))))
 
 (test environment-vars-should-override-existing
   (multiple-value-bind (status code)
       (external-program:run "which" '("which") :environment '(("PATH" . "")))
-    (is (eq status :exited))
-    (is (/= code 0))))
+    (is (eq :exited status))
+    (is (/= 0 code))))
 
 (test empty-env-should-erase-all
   (multiple-value-bind (status code)
       (external-program:run "which" '("which") :replace-environment-p t)
-    (is (eq status :exited))
-    (is (= code 71))))
+    (is (eq :exited status))
+    (is (= 71 code))))
