65b177eeb857c5920b1ec0e38d64be30fd55be8e
Greg Pfeil <greg@technomadic.org>
2011-01-15 14:35:57 -0500

    Revamped ECL's implementation and added environment support.

--- src/ecl.lisp
+++ src/ecl.lisp
@@ -3,29 +3,52 @@
 
 (in-package :external-program)
 
-;;;; No docs, but code is in ecl/src/c/unixsys.d
+;;; Outdated docs in http://ecls.sourceforge.net/new-manual/re11.html, but code
+;;; is in ecl/src/c/unixsys.d
 
 (defstruct external-process
   inputp
   outputp
   stream)
 
-(defmethod run (program args &key input output error &allow-other-keys)
+(defmethod run
+    (program args
+     &key input output error environment replace-environment-p
+     &allow-other-keys)
   (if (or output error)
       (warn "Can not control EXTERNAL-PROGRAM:RUN output in ECL."))
   (if input (error "Can not send input to EXTERNAL-PROGRAM:RUN in ECL."))
-  (values :exited (si:system (format nil "~s~{ ~a~}" program args))))
+  (when replace-environment-p
+    (setf args (append (list "-i" program "PATH=''") args))
+    (setf program "env"))
+  (values :exited
+          (nth-value 1
+                     (ext:run-program program (stringify-args args)
+                                      :wait t
+                                      :input input
+                                      :output output
+                                      :error error
+                                      :environ (reformat-environment
+                                                environment)))))
 
-(defmethod start (program args &key input output error &allow-other-keys)
+(defmethod start
+    (program args
+     &key input output error environment replace-environment-p
+     &allow-other-keys)
   (if (eq error :stream)
       (error "ECL can not create a stream for error output."))
+  (when replace-environment-p
+    (setf args (append (list "-i" program "PATH=''") args))
+    (setf program "env"))
   (make-external-process :inputp (eq input :stream)
                          :outputp (eq output :stream)
-                         :stream (ext:run-program program args
+                         :stream (ext:run-program program (stringify-args args)
                                                   :wait nil
                                                   :input input
                                                   :output output
-                                                  :error error)))
+                                                  :error error
+                                                  :environ (reformat-environment
+                                                            environment))))
 
 (defmethod process-input-stream (process)
   (if (external-process-inputp process)
