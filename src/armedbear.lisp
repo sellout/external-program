4e8a2b73f15bb8109f2da6f1c7ae0d531cad399e
Greg Pfeil <greg@technomadic.org>
2011-01-15 12:58:29 -0500

    Add environment support to ABCL.

--- src/armedbear.lisp
+++ src/armedbear.lisp
@@ -6,10 +6,13 @@
 ;;;; No docs, but code is in /Applications/abcl-0.0.9/src/org/armedbear/lisp/ShellCommand.java
 
 (defmethod run
-    (program args &key input output error &allow-other-keys)
+    (program args &key input output error environment replace-environment-p
+     &allow-other-keys)
   (when error
     (warn "Can not control EXTERNAL-PROGRAM:RUN error output in ABCL."))
   (when input (error "Can not send input to EXTERNAL-PROGRAM:RUN in ABCL."))
   (values :exited
-          (ext:run-shell-command (format nil "~s~{ ~a~}" program args)
+          (ext:run-shell-command (make-shell-string program args
+                                                    environment
+                                                    replace-environment-p)
                                  :output output)))
